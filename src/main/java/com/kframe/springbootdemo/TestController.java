e781660b87ddd200ca5efee24887b1e6f1906bc7
kunge2013 <543976974@qq.com>
2020-05-07 22:28:52 +0800

    1

--- src/main/java/com/kframe/springbootdemo/TestController.java
+++ src/main/java/com/kframe/springbootdemo/TestController.java
@@ -1,8 +1,10 @@
 package com.kframe.springbootdemo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @Controller
 public class TestController {
 
+//	DispatcherServlet
 }
