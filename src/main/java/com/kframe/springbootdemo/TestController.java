d31ed3bfb93aa33143412b8e35674118649a0a9f
fangkun <panzhangyi@squrab.com>
2020-05-08 11:05:16 +0800

    1

--- src/main/java/com/kframe/springbootdemo/TestController.java
+++ src/main/java/com/kframe/springbootdemo/TestController.java
@@ -1,13 +1,14 @@
 package com.kframe.springbootdemo;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class TestController {
-	
+	TomcatWebServer t;
 	@GetMapping("/hello")
 	public String hello() {
-		return "hello";
+		return "/hehe";
 	}
 }
