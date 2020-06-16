ee05639856f959aebac65c3a30f9d5302ef11b8f
Test Git <710784360@qq.com>
2020-06-16 20:31:10 +0800

    1

--- src/main/java/com/kframe/springbootdemo/TestController.java
+++ src/main/java/com/kframe/springbootdemo/TestController.java
@@ -1,5 +1,7 @@
 package com.kframe.springbootdemo;
 
+import javax.servlet.FilterChain;
+
 import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 	TomcatWebServer t;
+	FilterChain c;
 	@GetMapping("/hello")
 	public String hello() {
 		return "/hehe";
